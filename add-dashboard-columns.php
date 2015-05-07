028b468dee78dd49b7ac1914cac48256ebe3d841
23r9i0 <23r9i0@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-07 13:29:19 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/add-dashboard-columns/trunk@1155374 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- add-dashboard-columns.php
+++ add-dashboard-columns.php
@@ -3,37 +3,20 @@
  * Plugin Name: Add Dashboard Columns
  * Plugin URI: http://wordpress.org/plugins/add-dashboard-columns/
  * Description: Enable Dashboard Columns in WordPress after version 3.8
- * Version: 1.2
+ * Version: 1.3
  * Author: Sergio P.A. ( 23r9i0 )
  * Author URI: http://dsergio.com/
- * License: GPL2
- *
- *
- * Copyright 2014-2015  Sergio Prieto Alvarez  ( email : info@dsergio.com )
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) or exit;
 
-function add_dashboard_columns() {
-    add_screen_option( 'layout_columns', array( 'max' => 4, 'default' => 3 ) );
-}
-add_action( 'admin_head-index.php', 'add_dashboard_columns' );
+add_action( 'admin_head-index.php', function() {
+	add_screen_option( 'layout_columns', array( 'max' => 4, 'default' => 3 ) );
+} );
 
-function add_dashboard_columns_style( $hook ) {
-    if ( 'index.php' === $hook )
-        wp_enqueue_style( 'add-dashboard-columns', plugins_url( 'admin/css/add-dashboard-columns.min.css', __FILE__ ), array(), '1.2' );
-}
-add_action( 'admin_enqueue_scripts', 'add_dashboard_columns_style' );
+add_action( 'admin_enqueue_scripts', function( $hook ) {
+	if ( 'index.php' === $hook )
+		wp_enqueue_style( 'add-dashboard-columns', plugins_url( 'add-dashboard-columns.css', __FILE__ ), array(), '1.3' );
+} );
--- readme.txt
+++ readme.txt
@@ -3,15 +3,16 @@ Contributors: 23r9i0
 Donate link:
 Tags: dashboard, widget, dashboard columns, columns
 Requires at least: 3.8
-Tested up to: 4.1
-Stable tag: 1.2
-License: GPL/MIT
+Tested up to: 4.2.2
+Stable tag: 1.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Enable Dashboard Columns in WordPress after version 3.8
+Enable Dashboard Columns in WordPress 3.8 or later
 
 == Description ==
 
-Enable Dashboard Columns in WordPress after version 3.8
+Enable Dashboard Columns in WordPress 3.8 or later
 
 == Installation ==
 
@@ -19,5 +20,6 @@ Enable Dashboard Columns in WordPress after version 3.8
 * Activate the plugin through the 'Plugins' menu in WordPress.
 
 == Changelog ==
-= 1.2 =
-Current version
+= 1.3 =
+* Check support
+* clean directories/files
